06fc06e55c687dd6be2c0b0bc4f3babc8fec36d1
NotDannky <antonblanco58@gmail.com>
2024-01-09 21:09:08 +0100

    prueba

--- js_base/Gestion_Entidad.js
+++ js_base/Gestion_Entidad.js
@@ -22,7 +22,7 @@ class GestionEntidad {
 
     mostrarTitulos(){
 
-        this.ponerbotonesaddysearch(); // comprobar si ya existen
+        this.ponerbotonesaddysearch(); // comprobar si ya existen, pepe viyuela
 
         if (document.getElementById("titulostablacabecera")){
 
