4a2337bbed948d58a640cf666bcb11b0675155f7
NotDannky <antonblanco58@gmail.com>
2024-01-11 19:10:36 +0100

    traducciones de congreso

--- locales/Textos_ES.js
+++ locales/Textos_ES.js
@@ -10,7 +10,67 @@ let textos_ES={
     'controlador_invalido':'', // no se envia la entidad
     'action_invalido':'', // no se envia la accion
 
-   
+//Congreso-----------------------------------------------------------
+
+    "titulo_pagina_congreso": "Gestión de congreso",
+
+    //Atributos:
+   'codigo_congreso' :'Codigo del congreso',
+   'nombre_congreso' :'Nombre del congreso',
+   'acronimo_congreso' :'Acronimo del congreso',
+   'anho_congreso' :'Fecha del congreso',
+   'lugar_congreso' :'Lugar del congreso',
+   'ficheropodf_congreso' :'Fichero PDF del congreso',
+   'nuevo_ficheropdf_congreso' :'Nuevo fichero PDF del congreso',
+
+   'label_codigo_congreso' :'Codigo del congreso',
+   'label_nombre_congreso' :'Nombre del congreso',
+   'label_acronimo_congreso' :'Acronimo del congreso',
+   'label_anho_congreso' :'Fecha del congreso',
+   'label_lugar_congreso' :'Lugar del congreso',
+   'label_ficheropodf_congreso' :'Fichero PDF del congreso',
+   'label_nuevo_ficheropdf_congreso' :'Nuevo fichero PDF del congreso',
+
+    //Acciones:
+    'ADD': 'Añadir',
+    'SEARCH': 'Buscar',
+    'titulo_form_ADD_congreso': 'Añadir un congreso',
+    'titulo_form_EDIT_congreso': 'Editar un congreso',
+    'titulo_form_DELETE_congreso': 'Eliminar un congreso',
+    'titulo_form_SEARCH_congreso': 'Buscar un congreso',
+    'titulo_form_SHOWCURRENT_congreso': 'Información del congreso',
+
+    //Validaciones:
+    'KO_codigo_congreso_tam_min': 'El código del congreso no puede contener menos de 4 caracteres',
+    'KO_codigo_congreso_tam_max': 'El código del congreso no pede contener más de 11 caracteres',
+    'KO_codigo_congreso_numeros': 'El código del congreso solo puede contener dígitos',
+
+    'KO_nombre_congreso_tam_min': 'El nombre del congreso no puede contener menos de 2 caracteres',
+    'KO_nombre_congreso_tam_max': 'El nombre del congreso no pede contener más de 100 caracteres',
+    'KO_nombre_congreso_acentos_ñ_espacios': 'El nombre del congreso solo puede contener caracteres alfabéticos, con o sin acento, ñ y espacios',
+
+    'KO_acronimo_congreso_tam_min': 'El acronimo del congreso no puede contener menos de 2 caracteres',
+    'KO_acronimo_congreso_tam_max': 'El acronimo del congreso no puede contener más de 20 caracteres',
+    'KO_acronimo_congreso_no_acentos_ñ_espacios': 'El acronimo del congreso solo puede contener caracteres alfabéticos, sin acentos',
+
+    'KO_anho_congreso_tam_min': 'La fecha del congreso debe seguir el formato dd/mm/aaaa',
+    'KO_anho_congreso_tam_max': 'La fecha del congreso debe seguir el formato dd/mm/aaaa',
+    'KO_anho_congreso_formato_fecha_correcto': 'La fecha del congreso debe ser correcta',
+
+    'KO_lugar_congreso_tam_min': 'El lugar del congreso no puede contener menos de 2 caracteres',
+    'KO_lugar_congreso_tam_max': 'El lugar del congreso no pede contener más de 20 caracteres',
+    'KO_lugar_congreso_acentos_ñ_espacios': 'El lugar del congreso solo puede contener caracteres alfabéticos, con o sin acento, ñ y espacios',
+
+    'KO_ficheropdf_congreso_tam_min': 'El nombre del fichero PDF del congreso no puede contener menos de 1 caracter',
+    'KO_ficheropdf_congreso_tam_max': 'El nombre del fichero PDF del congreso no puede contener más de 20 caracteres',
+    'KO_ficheropdf_congreso_no_acentos_ñ_espacios_fich': 'El nombre del fichero PDF del congreso solo puede contener caracteres alfabéticos, . , \/ y \\',
+    'KO_ficheropdf_congreso_extension_fich_pdf': 'El fichero PDF del congreso debe tener la extensión .pdf',
+
+    'KO_nuevo_ficheropdf_congreso_tam_min': 'El nombre del nuevo fichero PDF del congreso no puede contener menos de 1 caracter',
+    'KO_nuevo_ficheropdf_congreso_tam_max': 'El nombre del nuevo fichero PDF del congreso no puede contener más de 20 caracteres',
+    'KO_nuevo_ficheropdf_congreso_no_acentos_ñ_espacios_fich': 'El nombre del nuevo fichero PDF del congreso solo puede contener caracteres alfabéticos, . , \/ y \\',
+    'KO_nuevo_ficheropdf_congreso_extension_fich_pdf': 'El nuevo fichero PDF del congreso debe tener la extensión .pdf',
+
 
 };
 
