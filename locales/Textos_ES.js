3d5761e9b723c77f1fddb37d2ae844ed844dfe0d
NotDannky <antonblanco58@gmail.com>
2024-01-11 19:40:54 +0100

    traducciones completas

--- locales/Textos_ES.js
+++ locales/Textos_ES.js
@@ -71,6 +71,96 @@ let textos_ES={
     'KO_nuevo_ficheropdf_congreso_no_acentos_ñ_espacios_fich': 'El nombre del nuevo fichero PDF del congreso solo puede contener caracteres alfabéticos, . , \/ y \\',
     'KO_nuevo_ficheropdf_congreso_extension_fich_pdf': 'El nuevo fichero PDF del congreso debe tener la extensión .pdf',
 
+//Usuario-----------------------------------------------------------
+
+    "titulo_pagina_usuario": "Gestión de usuario",
+
+    //Atributos:
+    'login_usuario': 'login del usuario',
+    'password_usuario': 'Contraseña del usuario',
+    'nombre_usuario': 'Nombre del usuario',
+    'apellidos_usuario': 'Apellidos el usuario',
+    'tituloacademico_usuario': 'Titulo académico del usuario',
+    'tipocontrato_usuario': 'Tipo de contrato del usuario',
+    'centro_usuario': 'Centro del usuario',
+    'departamento_usuario': 'Departamento del usuario',
+    'universidad_usuario': 'Universidad del usuario',
+    'foto_usuario': 'Foto del usuario',
+    'nueva_foto_usuario': 'Nueva foto del usuario',
+    'tipo_usuario': 'Tipo de usuario',
+
+    'label_login_usuario': 'login del usuario',
+    'label_password_usuario': 'Contraseña del usuario',
+    'label_nombre_usuario': 'Nombre del usuario',
+    'label_apellidos_usuario': 'Apellidos el usuario',
+    'label_tituloacademico_usuario': 'Titulo académico del usuario',
+    'label_tipocontrato_usuario': 'Tipo de contrato del usuario',
+    'label_centro_usuario': 'Centro del usuario',
+    'label_departamento_usuario': 'Departamento del usuario',
+    'label_universidad_usuario': 'Universidad del usuario',
+    'label_foto_usuario': 'Foto del usuario',
+    'label_nueva_foto_usuario': 'Nueva foto del usuario',
+    'label_tipo_usuario': 'Tipo de usuario',
+
+    //Acciones:
+    'ADD': 'Añadir',
+    'SEARCH': 'Buscar',
+    'titulo_form_ADD_usuario': 'Añadir un usuario',
+    'titulo_form_EDIT_usuario': 'Editar un usuario',
+    'titulo_form_DELETE_usuario': 'Eliminar un usuario',
+    'titulo_form_SEARCH_usuario': 'Buscar un usuario',
+    'titulo_form_SHOWCURRENT_usuario': 'Información del usuario',
+
+    //Validaciones:
+    'KO_login_usuario_tam_min': 'El login del usuario no puede contener menos de 4 caracteres',
+    'KO_login_usuario_tam_max': 'El login del usuario no puede contener más de 15 caracteres',
+    'KO_login_usuario_acentos_ñ_espacios_numeros': 'El login del usuario solo puede contener caracteres alfabéticos, con o sin acento, ñ, espacios y dígitos',
+
+    'KO_password_usuario_tam_min': 'El password del usuario no puede contener menos de 3 caracteres',
+    'KO_password_usuario_tam_max': 'El password del usuario no puede contener más de 32 caracteres',
+    'KO_password_usuario_acentos_ñ_espacios_numeros': 'El password del usuario solo puede contener caracteres alfabéticos, con o sin acento, ñ, espacios y dígitos',
+
+    'KO_nombre_usuario_tam_min': 'El nombre del usuario no puede contener menos de 2 caracteres',
+    'KO_nombre_usuario_tam_max': 'El nombre del usuario no pede contener más de 15 caracteres',
+    'KO_nombre_usuario_acentos_ñ_espacios': 'El nombre del usuario solo puede contener caracteres alfabéticos, con o sin acento, ñ y espacios',
+
+    'KO_apellidos_usuario_tam_min': 'Los apellidos del usuario no pueden contener menos de 3 caracteres',
+    'KO_apellidos_usuario_tam_max': 'Los apellidos del usuario no peden contener más de 30 caracteres',
+    'KO_apellidos_usuario_acentos_ñ_espacios': 'Los apellidos del usuario solo pueden contener caracteres alfabéticos, con o sin acento, ñ y espacios',
+
+    'KO_tituloacademico_usuario_tam_min': 'El titulo academico del usuario no puede contener menos de 5 caracteres',
+    'KO_tituloacademico_usuario_tam_max': 'El titulo academico del usuario no pede contener más de 100 caracteres',
+    'KO_tituloacademico_usuario_acentos_ñ_espacios_puntuacion': 'El titulo academico del usuario solo puede contener caracteres alfabéticos, con o sin acento, ñ, espacios y signos de puntuación',
+
+    'KO_tipocontrato_usuario_tam_min': 'El tipo de contrato del usuario no puede contener menos de 1 caracter',
+    'KO_tipocontrato_usuario_tam_max': 'El tipo de contrato del usuario no pede contener más de 40 caracteres',
+    'KO_tipocontrato_usuario_acentos_ñ_espacios_puntuacion': 'El tipo de contrato del usuario solo puede contener caracteres alfabéticos, con o sin acento, ñ, espacios y signos de puntuación',
+
+    'KO_centro_usuario_tam_min': 'El centro del usuario no puede contener menos de 5 caracteres',
+    'KO_centro_usuario_tam_max': 'El centro del usuario no pede contener más de 100 caracteres',
+    'KO_centro_usuario_acentos_ñ_espacios_puntuacion': 'El centro del usuario solo puede contener caracteres alfabéticos, con o sin acento, ñ, espacios y signos de puntuación',
+
+    'KO_departamento_usuario_tam_min': 'El departamento del usuario no puede contener menos de 5 caracteres',
+    'KO_departamento_usuario_tam_max': 'El departamento del usuario no pede contener más de 100 caracteres',
+    'KO_departamento_usuario_acentos_ñ_espacios': 'El departamento del usuario solo puede contener caracteres alfabéticos, con o sin acento, ñ y espacios',
+
+    'KO_universidad_usuario_tam_min': 'La universidad del usuario no puede contener menos de 5 caracteres',
+    'KO_universidad_usuario_tam_max': 'La universidad del usuario no pede contener más de 40 caracteres',
+    'KO_universidad_usuario_acentos_ñ_espacios': 'La universidad del usuario solo puede contener caracteres alfabéticos, con o sin acento, ñ y espacios',
+
+    'KO_foto_usuario_tam_min': 'El nombre de la foto del usuario no puede contener menos de 1 caracter',
+    'KO_foto_usuario_tam_max': 'El nombre de la foto del usuario no puede contener más de 20 caracteres',
+    'KO_foto_usuario_no_acentos_ñ_espacios_fich': 'El nombre de la foto del usuario solo puede contener caracteres alfabéticos, . , \/ y \\',
+    'KO_foto_usuario_extension_fich_jpg_jpeg': 'La foto del usuario debe tener la extensión .jpg o .jpeg',
+
+    'KO_nueva_foto_usuario_tam_min': 'El nombre de la nueva foto del usuario no puede contener menos de 1 caracter',
+    'KO_nueva_foto_usuario_tam_max': 'El nombre de la nueva foto del usuario no puede contener más de 20 caracteres',
+    'KO_nueva_foto_usuario_no_acentos_ñ_espacios_fich': 'El nombre de la nueva foto del usuario solo puede contener caracteres alfabéticos, . , \/ y \\',
+    'KO_nueva_foto_usuario_extension_fich_jpg_jpeg': 'La nueva foto del usuario debe tener la extensión .jpg o .jpeg',
+
+    "KO_tipo_usuario_formato":"El tipo de usuario solo puede ser uno de los valores permitidos (A o P)",
 
 };
+
+traduccion = textos_ES;
 
